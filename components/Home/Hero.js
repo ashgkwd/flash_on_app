627842f23ee2d4d0ba42bcfb0b4a1110702b0e55
Ashish Gaikwad <ash.gkwd@gmail.com>
2020-02-22 12:53:15 +0530

    makes layout of hero card

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -7,13 +7,17 @@ import {
   TouchableOpacity,
   View
 } from "react-native";
+import styled from "styled-components";
+
+const HeroCard = styled(View)`
+  height: 240px;
+  margin: 16px 8px;
+  background: black;
+  border-radius: 4px;
+`;
 
 function Hero() {
-  return (
-    <View>
-      <Text>Hero</Text>
-    </View>
-  );
+  return <HeroCard></HeroCard>;
 }
 
 export default Hero;
